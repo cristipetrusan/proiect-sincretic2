d0447ccd61ffa0c0f9a8efbfd44aba768207b5ad
cristipetrusan <george.cristian.petrusan@gmail.com>
2022-05-14 19:44:41 +0300

    implementing simulator

---
+++ WpfApp1/UserControl1.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        ViewModel _viewModel;
+        private System.Windows.Threading.DispatcherTimer _timer = new System.Windows.Threading.DispatcherTimer();
+        private const int _unit = 1;
+        public UserControl1()
+        {
+            InitializeComponent();
+            _viewModel = new ViewModel();
+            this.DataContext = _viewModel;
+            progressBar.Value = 50;
+            _timer.Interval = System.TimeSpan.FromMilliseconds(50);
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            GetProgressBarColor();
+            if (_viewModel.S1) progressBar.Value += 1;
+            if (_viewModel.S2) progressBar.Value += 1;
+            if (_viewModel.S3) progressBar.Value += 1;
+            if (_viewModel.S4) progressBar.Value += 1;
+            progressBar.Value -= 2;
+        }
+
+        private void GetProgressBarColor()
+        {
+            if (progressBar.Value >= 0 && progressBar.Value <= 70)
+            { progressBar.Foreground = new SolidColorBrush(Color.FromRgb(0, 255, 0)); return; }
+            if (progressBar.Value > 70 && progressBar.Value < 90)
+            { progressBar.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 255)); return; }
+            if (progressBar.Value > 90)
+            { progressBar.Foreground = new SolidColorBrush(Color.FromRgb(255, 0, 0)); return; }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _viewModel.S1 = !_viewModel.S1;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            _viewModel.S2 = !_viewModel.S2;
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.S3 = !_viewModel.S3;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            _viewModel.S4 = !_viewModel.S4;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            if ((_viewModel.S1 || _viewModel.S2 || _viewModel.S3 || _viewModel.S4)
+                && !(_viewModel.S1 && _viewModel.S2 && _viewModel.S3 && _viewModel.S4))
+            {
+                _viewModel.S1 = false;
+                _viewModel.S2 = false;
+                _viewModel.S3 = false;
+                _viewModel.S4 = false;
+                return;
+            }
+            _viewModel.S1 = !_viewModel.S1;
+            _viewModel.S2 = !_viewModel.S2;
+            _viewModel.S3 = !_viewModel.S3;
+            _viewModel.S4 = !_viewModel.S4;
+        }
+    }
+}
---
+++ WpfApp1/ViewModel.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    // public enum ProcessState
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string prop)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private bool _s1;
+        private bool _s2;
+        private bool _s3;
+        private bool _s4;
+
+        public bool S1
+        {
+            get { return _s1; }
+            set
+            {
+                _s1 = value;
+                OnPropertyChanged(nameof(S1Visibility));
+            }
+        }
+        public bool S2
+        {
+            get { return _s2; }
+            set
+            {
+                _s2 = value;
+                OnPropertyChanged(nameof(S2Visibility));
+            }
+        }
+        public bool S3
+        {
+            get { return _s3; }
+            set
+            {
+                _s3 = value;
+                OnPropertyChanged(nameof(S3Visibility));
+            }
+        }
+        public bool S4
+        {
+            get { return _s4; }
+            set
+            {
+                _s4 = value;
+                OnPropertyChanged(nameof(S4Visibility));
+            }
+        }
+        public System.Windows.Visibility S1Visibility
+        {
+            get
+            {
+                if (_s1)
+                    return System.Windows.Visibility.Visible;
+                else 
+                    return System.Windows.Visibility.Hidden;
+            }
+        }
+        public System.Windows.Visibility S2Visibility
+        {
+            get
+            {
+                if (_s2)
+                    return System.Windows.Visibility.Visible;
+                else
+                    return System.Windows.Visibility.Hidden;
+            }
+        }
+        public System.Windows.Visibility S3Visibility
+        {
+            get
+            {
+                if (_s3)
+                    return System.Windows.Visibility.Visible;
+                else
+                    return System.Windows.Visibility.Hidden;
+            }
+        }
+        public System.Windows.Visibility S4Visibility
+        {
+            get
+            {
+                if (_s4)
+                    return System.Windows.Visibility.Visible;
+                else
+                    return System.Windows.Visibility.Hidden;
+            }
+        }
+
+
+
+
+    }
+}
